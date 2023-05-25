e25ec0dd8ef93543a9fc365d0d219fe132220c14
miracles1919 <516571350@qq.com>
2023-05-25 08:52:18 +0800

    chore: update next app

--- next-13/app/page.tsx
+++ next-13/app/page.tsx
@@ -1,61 +1,58 @@
 'use client';
 
-import Image from 'next/image';
-import styles from './page.module.css';
-import { Button } from 'antd-mobile';
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Button, Popover } from 'antd-mobile';
+// import 'antd-mobile/es/global'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <Button>Button</Button>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href='https://beta.nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
+        
+        <div>
           <a
-            href='https://github.com/vercel/next.js/tree/canary/examples'
-            className={styles.card}
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
           </a>
         </div>
-      </main>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <Popover
+        content='Hello World'
+        trigger='click'
+        placement='right'
+      >
+        <Button color='primary'>Click Me</Button>
+      </Popover>
+
+    </main>
+  )
 }
